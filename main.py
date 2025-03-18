be7de8a4303f6551aa4570358736ea08fad0946a
Ryan Ramondetta <ryan.ramondetta@outlook.com>
2025-03-18 14:13:43 -0700

    outlining initial classes

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-#this will be my main
-print('hello world!')
+#main file
+
+#add libraries for color hexcodes
+
+#structure of a cube 
+class cube():
+   #cube size: default is 3x3x3
+   size = "3x3x3"
+
+   #more attributes here
+   Left = 
+
+
+   def __init__(self):
+      self.name 
+
+#class for a cube face
+
+#class for a cube node
+
+#function to map a whole side as a single color
